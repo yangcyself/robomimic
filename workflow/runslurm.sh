9613d2d4e118637d454e499004504e215faa438c
Chenyu Yang <chenyang@student.ethz.ch>
2023-05-29 18:27:13 +0200

    amend

--- workflow/runslurm.sh
+++ workflow/runslurm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #SBATCH -n 16
-#SBATCH --mem-per-cpu=512 ...
+#SBATCH --mem-per-cpu=512
 #SBATCH --time=8:00:00
 #SBATCH --tmp=0                        # per node!!
 #SBATCH --job-name=robomimic
+#SBATCH -A es_hutter
+#SBATCH --gpus=1
 
 python workflow/train.py --dataset /cluster/scratch/chenyang/May29_00-07-25/hdf_dataset.hdf5
 
