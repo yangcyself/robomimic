bacd0ce3cd5ec907240e0e6c0f1d4e20794148e2
Chenyu Yang <chenyang@student.ethz.ch>
2023-05-31 14:20:43 +0200

    subtle refine in launch script

--- workflow/runslurm.sh
+++ workflow/runslurm.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -n 16
 #SBATCH --mem-per-cpu=512
-#SBATCH --time=8:00:00
+#SBATCH --time=20:00:00  # IRIS takes too long
 #SBATCH --tmp=0                        # per node!!
 #SBATCH --job-name=robomimic
 #SBATCH -A es_hutter
